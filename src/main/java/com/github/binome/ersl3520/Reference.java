ada536ca3fcd82f1739835f9422c34916b1d3874
eroberts13 <erickaroberts426@gmail.com>
2015-11-09 13:44:01 -0600

    Updated Mod Name

--- src/main/java/com/github/binome/ersl3520/Reference.java
+++ src/main/java/com/github/binome/ersl3520/Reference.java
@@ -2,8 +2,8 @@ package com.github.binome.ersl3520;
 
 public class Reference
 {
-	public static final String MODID = "ersl";
-	public static final String NAME = "ERSL Mod";
+	public static final String MODID = "taniscraft";
+	public static final String NAME = "TanisCraft";
 	public static final String VERSION = "0.1";
 	public static final String COMMON_PROXY = "com.github.binome.ersl3520.proxies.CommonProxy";
 	public static final String CLIENT_PROXY = "com.github.binome.ersl3520.proxies.ClientProxy";
